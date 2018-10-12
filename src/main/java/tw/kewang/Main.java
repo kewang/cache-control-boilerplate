db7162d14db6137aa7246852e8853a6aa9b4c49d
kewang <cpckewang@gmail.com>
2018-10-12 23:06:41 +0800

    Refactor code

--- src/main/java/tw/kewang/Main.java
+++ src/main/java/tw/kewang/Main.java
@@ -4,12 +4,11 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
 import java.net.URI;
+import java.util.Scanner;
 
 /**
  * Main class.
- *
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
@@ -17,6 +16,7 @@ public class Main {
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     *
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
@@ -31,15 +31,22 @@ public class Main {
 
     /**
      * Main method.
+     *
      * @param args
-     * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
+    public static void main(String[] args) {
+        HttpServer server = startServer();
+
+        System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nHit 'close' to stop it...", BASE_URI));
+
+        String ret = "";
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!ret.equalsIgnoreCase("close")) {
+            ret = scanner.next();
+        }
+
         server.stop();
     }
 }
-
