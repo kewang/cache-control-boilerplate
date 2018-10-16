8b1efc32d52f0b4f4bf60268c722df4c9bdcc474
kewang <cpckewang@gmail.com>
2018-10-16 23:59:28 +0800

    Add CRUD operation (uncomplete)

---
+++ src/main/java/tw/kewang/ArticleResource.java
@@ -0,0 +1,37 @@
+package tw.kewang;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("articles")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ArticleResource {
+    private static final Logger LOG = LoggerFactory.getLogger(ArticleResource.class.getSimpleName());
+
+    @GET
+    public Response getArticles(@Context HttpHeaders httpHeaders) {
+        return Response.ok("{'a':'b'}").build();
+    }
+
+    @POST
+    public Response createArticle(@Context HttpHeaders httpHeaders, String body) {
+        return Response.ok("{'a':'b'}").build();
+    }
+
+    @DELETE
+    public Response removeArticle(@Context HttpHeaders httpHeaders, String body) {
+        return Response.ok("{'a':'b'}").build();
+    }
+
+    @PUT
+    public Response updateArticle(@Context HttpHeaders httpHeaders, String body) {
+        return Response.ok("{'a':'b'}").build();
+    }
+}
--- src/main/java/tw/kewang/Main.java
+++ src/main/java/tw/kewang/Main.java
@@ -11,37 +11,18 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Main class.
- */
 public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getSimpleName());
+    private static final String BASE_URI = "http://localhost:8080/api/";
 
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     *
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in tw.kewang package
+    private static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig().packages("tw.kewang");
 
         rc.register(new LoggingFeature(LOG, Level.INFO, Verbosity.PAYLOAD_ANY, null));
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-    /**
-     * Main method.
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         HttpServer server = startServer();
 
