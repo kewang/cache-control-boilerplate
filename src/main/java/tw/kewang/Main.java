248072b6f90976188fbf60cdfd01046d282e0eb5
kewang <cpckewang@gmail.com>
2018-10-16 23:38:27 +0800

    Add logger

--- src/main/java/tw/kewang/Main.java
+++ src/main/java/tw/kewang/Main.java
@@ -2,15 +2,21 @@ package tw.kewang;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.logging.LoggingFeature.Verbosity;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.net.URI;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main class.
  */
 public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getSimpleName());
+
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
 
@@ -23,6 +29,8 @@ public class Main {
         // create a resource config that scans for JAX-RS resources and providers
         // in tw.kewang package
         final ResourceConfig rc = new ResourceConfig().packages("tw.kewang");
+
+        rc.register(new LoggingFeature(LOG, Level.INFO, Verbosity.PAYLOAD_ANY, null));
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
